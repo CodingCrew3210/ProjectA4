c63f00930344ebd71ce87fcbde4d961dc78f3994
veraagyiri <60520429+veraagyiri@users.noreply.github.com>
2020-03-25 20:45:13 -0900

    Add files via upload

---
+++ deliverables/code/vera/fourth.s
@@ -0,0 +1,24 @@
+@This program compute the folloing if statement construct:
+@intx;
+@inty;
+@if(x == 0)
+@y = 1;
+.section .data
+x: .word 0			@32-bit signed integer, you can also use int
+				@directive instead of .word directive
+y: .word 0			@32-bit signed integer,
+.section .text
+.globl _start
+_start:
+  ldr r1, =x			@load the memory address of x into r1
+  ldr r1, [r1]			@load the value x into r1
+  cmp r1, #0			@
+  bne thenpart			@branch(jump) if true(Z == 0) to the thenpart
+  thenpart: mov r2, #1
+            ldr r3, =y		@load the memory address of y into r3
+            ldr r2, [r3]	@load r2 register value into y memory address
+	    mov r7, #1		@Program Termination: exit syscall
+	    svc #0		@Program Termination : wake kernel
+          .end
+
+
