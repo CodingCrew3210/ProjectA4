10f174b7d71899b8cdd42e843d49808cfec94a06
Hayden Kowalchuk <819028+mrneo240@users.noreply.github.com>
2020-03-27 19:29:18 -0400

    feat: finish up arm asm

--- deliverables/code/hkowalchuk/controlstructure2.s
+++ deliverables/code/hkowalchuk/controlstructure2.s
@@ -17,20 +17,19 @@ _start:
   ldr r1, =x    @ load the memory address of x into r1
   ldr r1, [r1]  @ load the value x into r1
 
+@!! NEW
+  mov r4, #1    @ r4 will be the value we subtract from x eventually, start at 1
+
   cmp r1, #3    @ the start of our logic for the if construct
-  bgt elsepart  @ the branch is taken if x > 3
-  thenpart:
-    sub r1, r1, #1 @ decrement r1, meaning x = x-1
-    ldr r2, =x     @ load the memory address of x into r2
-    str r1, [r2]   @ store r1 (x-1) register value into x memory address
-    b endofif
+  ble endofif   @ the branch is taken if x > 3
+ thenpart:
+    add r4, r4, #1 @ increment r4, now r4 = 2
 
-  elsepart:
-    sub r1, r1, #2 @ subtract 2 from x, x = x-2
+ endofif:
+    sub r1, r1, r4 @ subtract 1 or 2 from x, depending on the if check earlier
     ldr r2, =x	   @ load the memory address of x into r2
-    str r1, [r2]   @ store r1 (x-2) register value into x memory address
+    str r1, [r2]   @ store r1 register value into x memory address
 
-  endofif:
 @ Program termination
   mov r7, #1    @ Program Termination: exit syscall
   svc #0        @ Program Termination: wake kernel
